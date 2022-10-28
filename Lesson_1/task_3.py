b8996a621a53ea2f58e887787922956161e09266
Kowtun M <kowtyn.m@gmail.com>
2022-10-28 22:21:57 +0300

    first commit

--- Lesson_1/task_3.py
+++ Lesson_1/task_3.py
@@ -1,2 +1,2 @@
-print('\n\n#########', end='\n#\t\t#\n#\t\t#\n#\t\t#\n#########')
-print('\n\n#\t\t#\n#\t\t#\n#########', end='\n#\t\t#\n#\t\t#\n\n')
+print('\n\n#########', end='\n#\t\t#\n#\t\t#\n#\t\t#\n#########\n')
+print('\n#\t\t#\n#\t\t#\n#########', end='\n#\t\t#\n#\t\t#\n\n')
