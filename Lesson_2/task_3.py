c98687a720818a623df368d85b11ec4166fcb521
Kowtun M <kowtyn.m@gmail.com>
2022-10-29 21:16:05 +0300

    first commit

--- Lesson_2/task_3.py
+++ Lesson_2/task_3.py
@@ -1,8 +1,9 @@
 a = 2
-b = 4
+b = 6
 print(a + b)
-print(b - b)
-print(b ** 3)
+print(b - b + 1)
 print(b * a)
-print(b / a)
-print(b // a)
+print(b / a + 3)
+print(b // 3)
+print(b ** a)
+print(b % 4)
