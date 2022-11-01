ac73d52319fd393d6231d72cf25c09d2feb49d26
Kowtun M <kowtyn.m@gmail.com>
2022-11-01 17:46:33 +0200

    first commit

---
+++ lesson_4/task_2.py
@@ -0,0 +1,5 @@
+name = input('What is your name? ')
+age = input('How old are you? ')
+i = 1
+next_age = int(age) + i
+print(f"\nHello {name.title()}, on your next birthday you’ll be {next_age} years")
