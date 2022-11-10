933d589254a7c02161b4a4adacb12ead80d279e2
Kowtun M <kowtyn.m@gmail.com>
2022-11-10 13:15:26 +0200

    commit

--- Lesson_6/task_4.py
+++ Lesson_6/task_4.py
@@ -1,4 +1,6 @@
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 days_num = [1, 2, 3, 4, 5, 6, 7]
 days_l = dict(zip(days_num, days))
+days_o = dict(zip(days, days_num))
 print(days_l)
+print(days_o)
